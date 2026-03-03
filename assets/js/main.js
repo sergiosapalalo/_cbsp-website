d944c24f83fadb61c717098c5bc4c4fb4f2c7c09
sérgioSp <sergiosapalalo16@gmail.com>
2026-03-03 01:47:22 +0100

    created a menu mobile and started the home page

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+
+const btnMenu = document.querySelector('.btn-menu')
+const navList = document.querySelector('.nav-list')
+const overlay = document.querySelector('.overlay')
+const logoBtnSection = document.querySelector('.logo-btn')
+
+btnMenu.addEventListener('click', () => {
+    if (navList.classList.contains('showMenu')) {
+        navList.classList.remove('showMenu')
+        btnMenu.classList.remove('closeBtn')
+        navList.classList.remove('delay')
+        overlay.classList.remove('active')
+        logoBtnSection.classList.remove('addBorder')
+        document.body.classList.remove('no-scroll')
+
+    } else {
+        navList.classList.add('showMenu')
+        btnMenu.classList.add('closeBtn')
+        navList.classList.add('delay')
+        overlay.classList.add('active')
+        logoBtnSection.classList.add('addBorder')
+        document.body.classList.add('no-scroll')
+    }
+})
+
+overlay.addEventListener('click', () => {
+    navList.classList.remove('showMenu')
+    btnMenu.classList.remove('closeBtn')
+    navList.classList.remove('delay')
+    overlay.classList.remove('active')
+    logoBtnSection.classList.remove('addBorder')
+    document.body.classList.remove('no-scroll')
+
+})
